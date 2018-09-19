7824ff17b166cad0001e2c18df89baff861b6f7a
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-09-19 18:55:59 +0300

    Use FATAL logging level for gRpc logging info by default

--- plugin-bazel-event-service/src/main/kotlin/bazel/main.kt
+++ plugin-bazel-event-service/src/main/kotlin/bazel/main.kt
@@ -6,11 +6,15 @@ import bazel.v1.converters.BuildComponentConverter
 import bazel.v1.converters.StreamIdConverter
 import devteam.rx.subscribe
 import devteam.rx.use
+import org.apache.log4j.Level
 import java.io.IOException
 import java.util.logging.Logger
 
 @Throws(IOException::class, InterruptedException::class)
 fun main(args: Array<String>) {
+    org.apache.log4j.BasicConfigurator.configure()
+    org.apache.log4j.Logger.getRootLogger().level = Level.FATAL
+
     val logger = Logger.getLogger("main")
     val port: Int
     val verbosity: Verbosity
@@ -18,6 +22,9 @@ fun main(args: Array<String>) {
         val bazelOptions = BazelOptions(args)
         port = bazelOptions.port
         verbosity = bazelOptions.verbosity
+        if (verbosity == Verbosity.Trace) {
+            org.apache.log4j.Logger.getRootLogger().level = Level.DEBUG
+        }
     } catch (ex: Exception) {
         logger.severe(ex.message)
         BazelOptions.printHelp()
