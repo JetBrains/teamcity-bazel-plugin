9cb559baa5d2dbcc59a49c713bf77bbac80d4831
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2018-12-25 14:02:23 +0300

    TW-58466 Bloated build log in Bazel plugin - use the runner param "xmlReportParsing.quietMode" from xml report watcher to allow hide build log entries in the case with enormous numbers of test reports

---
+++ plugin-bazel-agent/src/main/kotlin/jetbrains/buildServer/bazel/RunnerInitializer.kt
@@ -0,0 +1,32 @@
+package jetbrains.buildServer.bazel
+
+import jetbrains.buildServer.agent.AgentLifeCycleAdapter
+import jetbrains.buildServer.agent.AgentLifeCycleListener
+import jetbrains.buildServer.agent.BuildRunnerContext
+import jetbrains.buildServer.agent.runner.ParameterType
+import jetbrains.buildServer.agent.runner.ParametersService
+import jetbrains.buildServer.util.EventDispatcher
+import jetbrains.buildServer.util.positioning.PositionAware
+import jetbrains.buildServer.util.positioning.PositionConstraint
+
+class RunnerInitializer(
+        events: EventDispatcher<AgentLifeCycleListener>)
+    : PositionAware, AgentLifeCycleAdapter() {
+
+    init {
+        events.addListener(this)
+    }
+
+    override fun beforeRunnerStart(runner: BuildRunnerContext) {
+        if (runner.runnerParameters[BazelConstants.PARAM_VERBOSITY]?.trim()?.let { Verbosity.tryParse(it) } != Verbosity.Diagnostic) {
+            // apply quiet mode for test xml reports watcher
+            runner.addRunnerParameter("xmlReportParsing.quietMode", "true")
+        }
+
+        super.beforeRunnerStart(runner)
+    }
+
+    override fun getOrderId(): String = ""
+
+    override fun getConstraint(): PositionConstraint = PositionConstraint.first()
+}
