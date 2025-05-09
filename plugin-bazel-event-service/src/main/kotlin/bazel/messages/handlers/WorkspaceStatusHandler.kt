972a9176db29bfe9a03c166c61751d978435355a
Vlad Ma-iu-shan <Vladislav.Ma-iu-shan@jetbrains.com>
2025-05-09 18:22:39 +0200

    drop comments

--- plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/WorkspaceStatusHandler.kt
+++ plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/WorkspaceStatusHandler.kt
@@ -19,15 +19,12 @@ class WorkspaceStatusHandler : EventHandler {
             if (ctx.event.payload is BazelEvent && ctx.event.payload.content is WorkspaceStatus) {
                 val event = ctx.event.payload.content
                 if (ctx.verbosity.atLeast(Verbosity.Verbose) && event.items.isNotEmpty()) {
-                    //ctx.onNext(ctx.messageFactory.createBlockOpened("Workspace Status", ""))
                     for (item in event.items) {
                         ctx.onNext(ctx.messageFactory.createMessage(
                                 ctx.buildMessage()
                                         .append(listOf(item.key, item.value).joinToStringEscaped(" = ").apply(Color.Items))
                                         .toString()))
                     }
-
-                    //ctx.onNext(ctx.messageFactory.createBlockClosed("Workspace Status"))
                 }
 
                 true
