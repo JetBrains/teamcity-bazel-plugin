8afaa7277344155a93058911293d03f09be163f8
Vlad Ma-iu-shan <Vladislav.Ma-iu-shan@jetbrains.com>
2025-05-21 17:26:23 +0200

    convert BazelEventHandler

--- plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/ComponentStreamFinishedHandler.kt
+++ plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/ComponentStreamFinishedHandler.kt
@@ -1,25 +1,23 @@
-
-
 package bazel.messages.handlers
 
 import bazel.HandlerPriority
 import bazel.Verbosity
 import bazel.atLeast
-import bazel.events.ComponentStreamFinished
-import bazel.events.FinishType
 import bazel.messages.Color
 import bazel.messages.ServiceMessageContext
 import bazel.messages.apply
+import com.google.devtools.build.v1.BuildEvent.BuildComponentStreamFinished.FinishType.EXPIRED
+import com.google.devtools.build.v1.BuildEvent.BuildComponentStreamFinished.FinishType.FINISHED
 
 class ComponentStreamFinishedHandler : EventHandler {
     override val priority: HandlerPriority
         get() = HandlerPriority.Low
 
     override fun handle(ctx: ServiceMessageContext) =
-        if (ctx.event.payload is ComponentStreamFinished) {
-            val componentStreamFinished = ctx.event.payload as ComponentStreamFinished
-            when (componentStreamFinished.finishType) {
-                FinishType.Finished ->
+        if (ctx.event.rawEvent.hasComponentStreamFinished()) {
+            val componentStreamFinished = ctx.event.rawEvent.componentStreamFinished
+            when (componentStreamFinished.type) {
+                FINISHED ->
                     if (ctx.verbosity.atLeast(Verbosity.Verbose)) {
                         val description = "Component \"${ctx.event.payload.streamId.component}\" stream finished"
                         ctx.onNext(
@@ -35,16 +33,19 @@ class ComponentStreamFinishedHandler : EventHandler {
                         )
                     }
 
-                FinishType.Expired ->
+                EXPIRED ->
                     if (ctx.verbosity.atLeast(Verbosity.Normal)) {
                         val description = "Component \"${ctx.event.payload.streamId.component}\" stream expired"
+                        val expiredDescription =
+                            "Set by the WatchBuild RPC server when the publisher of a build event stream stops" +
+                                " publishing events without publishing a BuildComponentStreamFinished event whose type equals FINISHED."
                         ctx.onNext(
                             ctx.messageFactory.createMessage(
                                 ctx
                                     .buildMessage()
                                     .append(description.apply(Color.Warning))
                                     .append(
-                                        "(${FinishType.Expired.description}), invocation: \"${ctx.event.payload.streamId.invocationId}\", build: \"${ctx.event.payload.streamId.buildId}\"",
+                                        "($expiredDescription), invocation: \"${ctx.event.payload.streamId.invocationId}\", build: \"${ctx.event.payload.streamId.buildId}\"",
                                         Verbosity.Verbose,
                                     ).toString(),
                             ),
--- plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/NotProcessedEventHandler.kt
+++ plugin-bazel-event-service/src/main/kotlin/bazel/messages/handlers/NotProcessedEventHandler.kt
@@ -4,9 +4,18 @@ package bazel.messages.handlers
 
 import bazel.HandlerPriority
 import bazel.messages.ServiceMessageContext
+import java.util.logging.Level
+import java.util.logging.Logger
 
 class NotProcessedEventHandler : EventHandler {
     override val priority: HandlerPriority get() = HandlerPriority.Last
 
-    override fun handle(ctx: ServiceMessageContext): Boolean = false
+    override fun handle(ctx: ServiceMessageContext): Boolean {
+        logger.log(Level.SEVERE, "Unknown event type: ${ctx.event}")
+        return false
+    }
+
+    companion object {
+        private val logger = Logger.getLogger(NotProcessedEventHandler::class.java.name)
+    }
 }
